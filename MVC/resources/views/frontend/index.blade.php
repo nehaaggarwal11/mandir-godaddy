f455fe13a8307ab44c8868279ee33c5b8415513f
Gursharan Singh <gursharan.smartdesignhut@gmail.com>
2021-09-07 15:59:24 +0530

    ready for godaddy

---
+++ MVC/resources/views/frontend/index.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.frontend')
+@section('css')
+<link rel="stylesheet" href="{{asset('assets/frontend/css/index.css')}}">
+@endsection
+
+@section('content')
+
+<div id="home-slider" class="carousel slide" data-ride="carousel">
+    <div class="container">
+        <div class="row">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{asset('assets/frontend/imgs/slider/luxmi-narayan.jpg')}}"
+                        class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}" class="d-block w-100">
+                </div>
+            </div>
+            <ol class="carousel-indicators">
+                <li data-target="#home-slider" data-slide-to="0" class="active"><img
+                        src="{{asset('assets/frontend/imgs/slider/luxmi-narayan.jpg')}}" class="d-block w-100">
+                </li>
+                <li data-target="#home-slider" data-slide-to="1"><img
+                        src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}" class="d-block w-100"></li>
+            </ol>
+             <a class="carousel-control-prev" href="#home-slider" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only d-none">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#home-slider" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only d-none">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+<section id="home-abt" class="sp mt-5">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <div class="mm-heading">Our Main Services</div>
+                <div class="seprator"></div>
+                <p>Although our temple has numerous departments performing various different services, the main focus is on the deity worship, prasadam distibution, goshala, devotee care and 24 hour hour kirtan of the holy names of the Lord, and all other functions carried out in the temple are all meant to support and encourage the betterment and success of these key services.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="gallery-filter" class="sp">
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-12 text-center">
+                <div class="mm-heading">Gallery</div>
+                <div class="seprator"></div>
+                <div class="filters filter-button-group">
+                    <ul>
+                        <li class="active" data-filter="*">All</li>
+                        <li data-filter=".webdesign">Charity</li>
+                        <li data-filter=".webdev">Construction</li>
+                        <li data-filter=".brand">Mandir Darshan</li>
+                        <li data-filter=".video">Videos</li>
+                    </ul>
+                </div>
+                <div class="content grid">
+                <div class="single-content webdesign webdev grid-item">
+                        <img class="p2" src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}">
+                </div>
+                <div class="single-content brand webdesign grid-item">
+                        <img class="p2" src="{{asset('assets/frontend/imgs/slider/luxmi-narayan.jpg')}}">
+                </div>
+                <div class="single-content brand grid-item">
+                        <img class="p2" src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}">
+                </div>
+                    <div class="single-content webdesign grid-item">
+                        <img class="p2" src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}">
+                </div>
+                    <div class="single-content webdesign grid-item">
+                        <img class="p2" src="{{asset('assets/frontend/imgs/gallery/g1.jpg')}}">
+                </div>
+                <div class="single-content webdesign brand grid-item">
+                        <img class="p2"src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}">
+                </div>
+                <div class="single-content webdesign grid-item">
+                        <img class="p2" src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}">
+                </div>
+                <div class="single-content webdesign webdev grid-item">
+                        <img class="p2"src="{{asset('assets/frontend/imgs/slider/slider-demo.jpg')}}">
+                </div>
+                <div class="single-content webdesign video grid-item">
+                    <iframe width="100%" height="235" src="https://www.youtube.com/embed/gZllwhG_MCc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+@section('script')
+<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+<script>
+$(window).on('load',function(){
+     $('.grid').isotope({
+    itemSelector: '.grid-item',
+    });
+});
+ $(document).ready(()=>{
+    // filter items on button click
+        $('.filter-button-group').on( 'click', 'li', function() {
+        var filterValue = $(this).attr('data-filter');
+        $('.grid').isotope({ filter: filterValue });
+        $('.filter-button-group li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+ });
+</script>
+@endsection
---
+++ MVC/resources/views/layouts/frontend.blade.php
@@ -0,0 +1,64 @@
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    @yield('meta_tags')
+    <!-- Favicons -->
+    <link href="{{asset('assets/frontend/imgs/logo/favicon.png')}}" rel="icon">
+    <link href="{{asset('assets/frontend/imgs/logo/favicon.png')}}" rel="apple-touch-icon">
+    <!--------- CSS ------->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('assets/frontend/css/style.css')}}">
+     @yield('css')
+    <!------- JS ---------->
+    <script  src="https://code.jquery.com/jquery-3.6.0.min.js"  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    {{-- <script src="/js/app.js"></script> --}}
+</head>
+<body>
+<header>
+    <nav class="navBar">
+       <div class="row">
+            <div class="col-md-4 text-right">    
+                <div class="mm-head">Mandir Murti</div>           
+            </div>
+            <div class="col-md-4 text-center">
+                <a href="/" class="logo-anchor"><img alt="logo" class="img-fluid" width="150" height="150" src="{{asset('assets/frontend/imgs/logo/mandir-logo-brown3.png')}}"></a>
+            </div>
+            <div class="col-md-4 text-left">
+                <div class="mm-head">Luxmi Narayan</div> 
+            </div>
+       </div>
+    </nav>
+</header>
+
+@yield('content')
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div id="footer-content">
+                    <p>Mandir Murti Luxmi Narayan is devoted towards the deity worship and charities. All other functions carried out in the temple are all meant to support and encourage the betterment and success of the society.</p>
+                    <p id="copy-right">Copyright © 2021 <a href="www.mandirmurtiluxminarayan.com" class="text-light">mandirmurtiluxminarayan.com</a>. All rights reserved</p>
+                </div>   
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!------ Image lightbox ---->
+<div id="lightBox">
+    <div class="lightBox-bg">
+        <div class="close"></div>
+        <div class="lightBox-body">
+
+        </div>
+    </div>
+</div>
+<!------ END Image lightbox ---->
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+@yield('script')
+</body>
+</html>
